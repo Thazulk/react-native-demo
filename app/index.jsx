b4e916f11cf2c4b1bebb5cd682e19af6c40b2860
Thazulk <thazulk@gmail.com>
2025-08-05 11:20:24 +0200

    feat(styling): update app configuration and enhance Home component with logo and styling

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image  } from 'react-native'
 import React from 'react'
+import Logo from '../assets/icon.png'
 
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+const Home = () => (
+  <View style={styles.container}>
+
+ <Image source={Logo} style={styles.img} />
+
+    <Text style={styles.title}>The Number 1</Text>
+    <Text style={{marginTop: 10, marginBottom:30}}>Reading List App</Text>
+
+  </View>
+
+
+
+)
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  img: {
+    width: 100,
+    height: 100,
+    marginVertical: 20,
+  },
+})
